3b83cce01c2ff810f2577d7e1387067298a410f7
Virendra115 <mvirendra115@google.com>
2022-12-02 00:57:27 +0530

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
+import java.util.Collections.sort
+import java.util.Comparator
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val list = input.split("\r\n\r\n").map { a -> a.split("\r\n").sumOf { it.toInt() } }
+        return list.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        val list = input.split("\r\n\r\n").map { a -> a.split("\r\n").sumOf { it.toInt() } }
+        sort(list, Comparator.reverseOrder())
+        return list[0] + list[1] + list[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
